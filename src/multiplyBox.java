5f00c695781d224c0386c150a89ddbe243d5ee62
Gabriel Sousa <gabriel2001@live.com.pt>
2022-11-15 11:36:09 +0000

    Commit error

--- src/multiplyBox.java
+++ src/multiplyBox.java
@@ -1,9 +1,13 @@
 public class multiplyBox {
-    private static int[] boxDigits = {1, 4, 7, 8};
+    private static int[] boxDigits = {};
 
     public static int multiply() {
         int total = 1;
-        for (int i = 1; i < boxDigits.length; i++) {
+        if (boxDigits.length == 0){
+            return 0;
+        }
+        for (int i = 0; i < boxDigits.length; i++) {
+
             total = total * boxDigits[i];
         }
         return total;
